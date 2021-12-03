31b30cb5306b8d51f5de3dec2bd552425b4edc2a
StakhovDmitry <91256329+StakhovDmitry@users.noreply.github.com>
2021-12-03 07:45:37 +0300

    Update DZ-4proj.c

--- DZ-4proj/DZ-4proj/DZ-4proj.c
+++ DZ-4proj/DZ-4proj/DZ-4proj.c
@@ -12,9 +12,11 @@ int main(void) {
     scanf_s("%d %d", &ver, &reb);
     int* arr = (int*)malloc(ver * reb * sizeof(int));
     bool* flag = (bool*)malloc(reb * sizeof(bool));
+    
     for (int i = 0; i < reb; i++) {
         flag[i] = false;
     }
+    
     for (int i = 0; i < reb; i++) {
         printf("%d:", i);
 
@@ -22,7 +24,9 @@ int main(void) {
             scanf_s("%d", (arr + i * ver + j));
         }
     }
+    
     printf("\n");
+    
     for (int i = 0; i < reb; i++) {
 
         for (int j = 0; j < ver; j++) {
@@ -34,13 +38,17 @@ int main(void) {
         }
         count = 0;
     }
+    
     scanf_s("%d", &step);
+    
     for (int j = 0; j < ver; j++) {
+        
         for (int i = 0; i < reb; i++) {
             count = count + arr[i * ver + j];
         }
         if (count == step) {
             for (int k = 0; k < reb; k++) {
+                
                 if ((arr[k * ver + j] != 0) && (flag[k] == false)) {
                     printf("%d", k);
                     flag[k] = true;
@@ -51,21 +59,3 @@ int main(void) {
     }
 }
 
-
-
-
-
-
-
-
-
-
-    //for (int i = 0; i < reb; i++) {
-        //printf("%d:", i);
-
-        //for (int j = 0; j < ver; j++) {
-           // printf("%d ", arr[i * reb + j]);
-       // }
-        //printf("\n");
-   // }
-   // printf("\n");
